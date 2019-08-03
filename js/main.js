7c42e1534d76a61dcbaf1e65e6928f7158d767a6
Elco Garcia <garciaelco@gmail.com>
2019-08-03 15:14:10 -0500

    added random number function to grid

--- js/main.js
+++ js/main.js
@@ -1,18 +1,32 @@
 /*----- constants -----*/
 
 /*----- app's state (variables) -----*/
-
+let grid;
 /*----- cached element references -----*/
-//scorebaord player 1 and players 2
+
 /*----- event listeners -----*/
-//keyboard up, down, left, and right arrows
+
 /*----- functions -----*/
-
-// function for generating random number between 2 or 4 on the board
-function randomNumberBoard() {
-	let number = Math.random() < 0.5 ? 2 : 4;
-
-	return number;
+init();
+function init() {
+	grid = [ [ 0, 0, 0, 0 ], [ 0, 0, 0, 0 ], [ 0, 0, 0, 0 ], [ 0, 0, 0, 0 ] ];
+	console.table(grid);
+	addTwoOrFour();
+	addTwoOrFour();
+	console.table(grid);
 }
 
-//function add numbers when they pushed in a direction
+function addTwoOrFour() {
+	let options = [];
+	for (let i = 0; i < 4; i++) {
+		for (let j = 0; j < 4; j++) {
+			if (grid[i][j] === 0) {
+				options.push({ x: i, y: j });
+			}
+		}
+	}
+	if (options.length > 0);
+	let randomSpot = options[Math.floor(Math.random() * options.length)];
+	let number = Math.random() < 0.5 ? 2 : 4;
+	grid[randomSpot.x][randomSpot.y] = number;
+}
